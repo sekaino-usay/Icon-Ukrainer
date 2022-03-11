98fb16756d60b3e20764e54e91ad77a5fd4ea588
U_SAY <sekaino.usay@gmail.com>
2022-03-11 11:55:52 +0900

    Initial Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Icon Ukrainer</title>
+</head>
+
+<body>
+    <form action="./ukrainer.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="image" accept="image/*">
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
